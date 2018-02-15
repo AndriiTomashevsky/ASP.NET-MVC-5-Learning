3a4f0c4cb82f4960db1c2666566aceba6b61aeaf
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-02-16 01:54:06 +0200

    Commit 1 -  Отображение списка товаров имитированного хранилища.

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        // GET: Product
+        public ViewResult List()
+        {
+            IEnumerable<Product> model = productRepository.Products;
+
+            return View(model);
+        }
+    }
+}
