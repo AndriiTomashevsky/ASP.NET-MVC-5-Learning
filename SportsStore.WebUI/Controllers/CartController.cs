272ad1e45e652abc0c8624b2d4e898c400dfb896
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-02-19 17:56:24 +0200

    Commit 2 - Отображение содержимого корзины.

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -12,6 +13,15 @@ namespace SportsStore.WebUI.Controllers
         public CartController(IProductRepository repository)
         {
             this.repository = repository;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndxeViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
         }
 
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
