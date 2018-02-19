e04fc35fd6fbedf698233b898068b6a72b6e3ace
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-02-19 23:06:59 +0200

    Commit 1 - Админ панель: список товаров, тестирование: метод действия Index()

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository productRepository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(productRepository.Products);
+        }
+    }
+}
